5fda4b20ec4e47909202e3d66a2086da0e7a3f88
Redwan <rethawan2007@gmail.com>
2021-11-15 14:09:29 +0300

    V 0.2 Added proper logging

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ root.title = 'Rock/paper/scissors'
 
 scr = Label(root, text='Welcome to the game!')
 
-possible = ['rock','paper','scissors']
+possible = ["rock","paper","scissors"]
 
 log = {}
 
@@ -20,33 +20,37 @@ def choose(input):
 
 def pc_choose():
     global pc_choice
-    pc_choice = ''.join(random.choices(possible, k=1))
+    pc_choice = "".join(random.choices(possible, k=1))
     print(pc_choice)
     compare()
 
 def compare():
     global scr
     if pc_choice == choice:
-        text = 'Tied!'
+        text = "Tied!"
     elif possible.index(pc_choice) > possible.index(choice):
-        text = 'You lose!'
-    elif pc_choice == 'rock' and choice == 'scissors':
-        text = 'You lose!'
+        text = "You lose!"
+    elif pc_choice == "rock" and choice == 'scissors':
+        text = "You lose!"
     else:
-        text = 'You win!'
-    scr.grid_forget()
-    scr = Label(root, text=text)
-    scr.grid(row=0,column=0, columnspan=3)
-    log.setdefault(time.time(),(choice,pc_choice,text))
+        text = "You win!"
+    now = time.strftime("%d/%m/%Y %H:%M:%S")
+    scr.config(text=text)
+    log.setdefault(now,[choice,pc_choice,text])
 
-rock = Button(root, padx=10, pady=10, command=lambda: choose('rock'), text='Rock')
-paper = Button(root, padx=10, pady=10, command=lambda: choose('paper'), text='Paper')
-scissors = Button(root, padx=10, pady=10, command=lambda: choose('scissors'), text='Scissors')
+rock = Button(root, padx=10, pady=10, command=lambda: choose("rock"), text='Rock')
+paper = Button(root, padx=10, pady=10, command=lambda: choose("paper"), text='Paper')
+scissors = Button(root, padx=10, pady=10, command=lambda: choose("scissors"), text='Scissors')
+
+send = Button (root, padx=30, command=pc_choose, text='Start!')
 
 scr.grid(row=0,column=0, columnspan=3)
 rock.grid(row=1,column=0)
 paper.grid(row=1,column=1)
 scissors.grid(row=1,column=2)
+send.grid(row=2,column=0,columnspan=3)
 
 if __name__ == '__main__':
     root.mainloop()
+    with open('./log.json','w') as f:
+        f.write(str(log).replace('\'','"'))
